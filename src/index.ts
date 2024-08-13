1fa1329c2d498b6e00f0747770640640e5e02b44
n-mazaheri <95586576+n-mazaheri@users.noreply.github.com>
2024-08-14 01:44:32 +0330

    fix(index): export modules

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
-import TransactionGraph from './components/TransactionGraph';
 import { useTransactionData } from './hooks/useTransactionData';
-
-export { TransactionGraph, useTransactionData };
+import TransactionsByAddress from './components/barcharts/TransactionsByAddress';
+import TransactionsByToken from './components/barcharts/TransactionsByToken';
+import TransactionsByType from './components/barcharts/TransactionsByType';
+import TransactionsInOut from './components/barcharts/TransactionsInOut';
+import DirectedGraph from './components/graph/DirectedGraph';
+import AccumulatedValueOverTime from './components/lineCharts/AccumulatedValueOverTime';
+import AvgTransactionValueOverTime from './components/lineCharts/AvgTransactionValueOverTime';
+import TransactionsOverTime from './components/lineCharts/TransactionsOverTime';
+export {
+  useTransactionData,
+  TransactionsByAddress,
+  TransactionsByToken,
+  TransactionsByType,
+  TransactionsInOut,
+  DirectedGraph,
+  AccumulatedValueOverTime,
+  AvgTransactionValueOverTime,
+  TransactionsOverTime,
+};
